65d959f731724fae42e962b8de990927d4fe0940
BRKMCHN <135169203+BRKMCHN@users.noreply.github.com>
2023-10-14 15:49:45 -0400

    added 2 scripts

---
+++ RA_Main_Scripts/RA-Keep_selected_items_looped_longer_than_source.lua
@@ -0,0 +1,24 @@
+-- Start the Undo block
+reaper.Undo_BeginBlock()
+
+-- Loop through all selected items
+local numSelectedItems = reaper.CountSelectedMediaItems(0)
+for i = numSelectedItems-1, 0, -1 do
+    local item = reaper.GetSelectedMediaItem(0, i)
+    if item then
+        local take = reaper.GetActiveTake(item)
+        if take and not reaper.TakeIsMIDI(take) then -- Ensure it's not a MIDI take
+            local itemLength = reaper.GetMediaItemInfo_Value(item, "D_LENGTH")
+            local source = reaper.GetMediaItemTake_Source(take)
+            local sourceLength = reaper.GetMediaSourceLength(source)
+            if itemLength <= sourceLength then
+                -- Deselect the item if its length is not longer than its source
+                reaper.SetMediaItemSelected(item, false)
+            end
+        end
+    end
+end
+
+-- End the Undo block
+reaper.Undo_EndBlock("Select items longer than their source", -1)
+
---
+++ RA_Main_Scripts/RA-Reduce_Take_Gain_by_a_Quarter.lua
@@ -0,0 +1,43 @@
+-- Start the Undo block
+reaper.Undo_BeginBlock()
+
+-- Convert a multiplier to dB
+function lin2dB(val)
+    return 20 * math.log(val, 10)
+end
+
+-- Convert dB to a multiplier
+function dB2lin(dB_val)
+    return 10^(dB_val / 20)
+end
+
+-- Loop through all selected items
+local numSelectedItems = reaper.CountSelectedMediaItems(0)
+for i = 0, numSelectedItems-1 do
+    local item = reaper.GetSelectedMediaItem(0, i)
+    if item then
+        local take = reaper.GetActiveTake(item)
+        if take then
+            -- Fetch the current gain value of the take (multiplier)
+            local currentGain = reaper.GetMediaItemTakeInfo_Value(take, "D_VOL")
+            -- Convert the current multiplier to dB
+            local currentGain_dB = lin2dB(currentGain)
+            
+            -- Simply decrease the dB gain by multiplying with 0.75
+            currentGain_dB = currentGain_dB * 0.75
+            
+            -- Convert the adjusted dB value back to multiplier
+            local adjustedGain = dB2lin(currentGain_dB)
+            
+            -- Set the adjusted gain to the take
+            reaper.SetMediaItemTakeInfo_Value(take, "D_VOL", adjustedGain)
+        end
+    end
+end
+
+-- Update arrange view and redraw
+reaper.UpdateArrange()
+
+-- End the Undo block
+reaper.Undo_EndBlock("Reduce take gain by 75%", -1)
+
