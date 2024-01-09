63a390e35f505ca60e3c09620866a7d903705be6
BRKMCHN <135169203+BRKMCHN@users.noreply.github.com>
2024-01-09 11:31:11 -0500

    Update RA-Move selected items to first selected track within overlap threshold.lua
    Added a check for full item overlaps to avoid tiny items sneaking in.

--- RA_Main_Scripts/RA-Move selected items to first selected track within overlap threshold.lua
+++ RA_Main_Scripts/RA-Move selected items to first selected track within overlap threshold.lua
@@ -1,4 +1,4 @@
--- @version 1.0
+-- @version 1.1
 -- @description MOVE SELECTED ITEMS to FIRST SELECTED TRACK. (Within an overlap threshold)
 -- @author RESERVOIR AUDIO / MrBrock adapted with AI.
 -- @about This script will apply to selected items and check against themselves. The first selected track is the destination for the end result. On line 5, in the "check for overlaps" part of the script, the initial value of 0.3 means 300ms. You may change this to your preferred maximum overlap allowed.
@@ -9,6 +9,11 @@ reaper.Undo_BeginBlock()
 -- Function to calculate overlap amount between two ranges
 local function overlapAmount(start1, end1, start2, end2)
     return math.max(0, math.min(end1, end2) - math.max(start1, start2))
+end
+
+-- Function to calculate the length of a media item
+local function itemLength(item)
+    return reaper.GetMediaItemInfo_Value(item, "D_LENGTH")
 end
 
 -- Get the number of selected media items
@@ -35,21 +40,22 @@ local items = {}
 for i = 0, numSelectedItems-1 do
     local item = reaper.GetSelectedMediaItem(0, i)
     local st = reaper.GetMediaItemInfo_Value(item, "D_POSITION")
-    local en = st + reaper.GetMediaItemInfo_Value(item, "D_LENGTH")
+    local en = st + itemLength(item)
     items[#items+1] = {
         it = item,
         st = st,
-        en = en
+        en = en,
+        len = itemLength(item)
     }
 end
 
--- Check for overlaps
+-- Check for overlaps and full length overlaps
 for i, srcItem in ipairs(items) do
     local canMove = true
     for j, otherItem in ipairs(items) do
         if i ~= j then
             local overlap = overlapAmount(srcItem.st, srcItem.en, otherItem.st, otherItem.en)
-            if overlap > 0.3 then
+            if overlap > 0.3 or overlap >= srcItem.len then
                 canMove = false
                 break
             end
@@ -62,4 +68,3 @@ end
 
 reaper.UpdateArrange()
 reaper.Undo_EndBlock("Move selected items to selected track without overlapping more than 300ms", -1)
-
