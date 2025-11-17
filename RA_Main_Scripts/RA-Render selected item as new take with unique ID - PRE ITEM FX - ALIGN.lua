89e943d8ca45f86315345b9b2d6a4ece73d40d30
BRKMCHN <135169203+BRKMCHN@users.noreply.github.com>
2025-11-17 11:05:17 -0500

    updated version on align version of render as new take

--- RA_Main_Scripts/RA-Render selected item as new take with unique ID - PRE ITEM FX - ALIGN.lua
+++ RA_Main_Scripts/RA-Render selected item as new take with unique ID - PRE ITEM FX - ALIGN.lua
@@ -1,5 +1,5 @@
 -- @description Render selected item as new take with unique ID - PRE ITEM FX - Set for RA auto align
--- @version 1.2
+-- @version 1.3
 -- @author
 --   RESERVOIR AUDIO / MrBrock with AI
 
