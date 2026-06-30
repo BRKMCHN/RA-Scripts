8184a815246556668c24610490ebc5712a3ac7e2
BRKMCHN <135169203+BRKMCHN@users.noreply.github.com>
2026-06-30 14:08:35 -0400

    added snap toggle script

---
+++ RA_Main_Scripts/RA-Toggle Item Snap Mode.lua
@@ -0,0 +1,64 @@
+-- @version 1.0
+-- @description Toggle Item Snap Mode
+-- @author RESERVOIR AUDIO / MrBrock + AI
+
+-- Toggle Snap/Grid setting:
+-- Media items snap at: "Only snap at start/snap offset" <-> "Snap both start/end"
+--
+-- Requires SWS extension.
+
+local VAR_NAME = "projshowgrid"
+
+local BIT_START_ONLY = 2048
+local BIT_BOTH_ENDS  = 4096
+
+local function has_bit(value, bit)
+    return value & bit ~= 0
+end
+
+local function remove_bit(value, bit)
+    if has_bit(value, bit) then
+        return value - bit
+    end
+    return value
+end
+
+if not reaper.SNM_GetIntConfigVar or not reaper.SNM_SetIntConfigVar then
+    reaper.ShowMessageBox(
+        "This script requires the SWS extension.",
+        "Missing SWS",
+        0
+    )
+    return
+end
+
+local current = reaper.SNM_GetIntConfigVar(VAR_NAME, -999999)
+
+if current == -999999 then
+    reaper.ShowMessageBox(
+        "Could not read REAPER snap/grid setting: " .. VAR_NAME,
+        "Error",
+        0
+    )
+    return
+end
+
+local currently_both = has_bit(current, BIT_BOTH_ENDS)
+
+-- Clear both dropdown bits first.
+local new_value = current
+new_value = remove_bit(new_value, BIT_START_ONLY)
+new_value = remove_bit(new_value, BIT_BOTH_ENDS)
+
+if currently_both then
+    -- Switch to: Only snap at start/snap offset
+    new_value = new_value + BIT_START_ONLY
+else
+    -- Switch to: Snap both start/end
+    new_value = new_value + BIT_BOTH_ENDS
+end
+
+reaper.SNM_SetIntConfigVar(VAR_NAME, new_value)
+
+reaper.UpdateTimeline()
+reaper.UpdateArrange()
